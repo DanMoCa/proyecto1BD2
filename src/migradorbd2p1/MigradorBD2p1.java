adc02592ed98c654b773649320491cb093860aac
Dan <Pankexillo.lerolero@gmail.com>
2014-09-26 15:45:54 -0600

    Conexión a Oracle exitosa.

---
+++ src/migradorbd2p1/MigradorBD2p1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package migradorbd2p1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MigradorBD2p1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            Connection conn = Conexion.GetConnection();
+            String sql = "SELECT * FROM employees";
+            PreparedStatement ps;
+            ResultSet rs;
+            
+            ps = conn.prepareStatement(sql);
+            
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+                System.out.println(rs.getString(1)+ " " + rs.getString(2));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MigradorBD2p1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }
+    
+}
