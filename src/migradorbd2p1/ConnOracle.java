c4189425edf18130e147f87c9d6242523e7760f3
Angek <Angek@Angek-PC>
2014-09-27 20:13:35 -0600

    se agrego funciones para el logueo de oracle. se modifico main para hacer la estructura de los datos. se creo una funcion basica para ordenar los datos de una tabla.

--- src/migradorbd2p1/ConnOracle.java
+++ src/migradorbd2p1/ConnOracle.java
@@ -16,7 +16,27 @@ import javax.swing.JOptionPane;
  * @author Kevin Arnold
  */
 public class ConnOracle {
-      
+    private static String usuarioDB="dan";
+    private static String passwordDB="1234";
+    private static String serv="XE";
+    public String getUsuarioDB() {
+        return usuarioDB;
+    }
+
+    public static void setUsuarioDB(String usuarioDB) {
+        ConnOracle.usuarioDB = usuarioDB;
+    }
+
+    public static String getPasswordDB() {
+        return passwordDB;
+    }
+
+    public static void setPasswordDB(String passwordDB) {
+        ConnOracle.passwordDB = passwordDB;
+    }
+    
+    
+    
     public static Connection GetConnection()
     {
         Connection conexion=null;
@@ -24,14 +44,15 @@ public class ConnOracle {
         try
         {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            String servidor = "jdbc:oracle:thin:@localhost:1521:XE";
-            String usuarioDB="dan";
-            String passwordDB="1234";
+            String servidor = "jdbc:oracle:thin:@localhost:1521:"+serv;
+            
+            System.out.println(servidor+usuarioDB+passwordDB+serv);
             conexion= DriverManager.getConnection(servidor,usuarioDB,passwordDB);
+            
         }
         catch(ClassNotFoundException ex)
         {
-            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD "+ex.getMessage()+"\n", JOptionPane.ERROR_MESSAGE);
             conexion=null;
         }
         catch(SQLException ex)
@@ -48,5 +69,13 @@ public class ConnOracle {
         {
             return conexion;
         }
+    }
+
+    public static String getServ() {
+        return serv;
+    }
+
+    public static void setServ(String serv) {
+        ConnOracle.serv = serv;
     }
 }
--- src/migradorbd2p1/MigradorBD2p1.java
+++ src/migradorbd2p1/MigradorBD2p1.java
@@ -12,6 +12,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.sql.CallableStatement;
+import oracle.jdbc.OracleTypes;
 
 public class MigradorBD2p1 {
 
@@ -21,25 +24,81 @@ public class MigradorBD2p1 {
     public static void main(String[] args) {
         try {
             // TODO code application logic here
-            Connection conn = ConnSQLServer.GetConnection();
-            String sql = "SELECT * FROM tbusuarios";
-            PreparedStatement ps;
-            ResultSet rs;
+            if(JOptionPane.showConfirmDialog(null, "¿Es usted el usuario dan?")!=0)
+            {
+                
+                ConnOracle.setServ(JOptionPane.showInputDialog(null, "Inserte el valor del servicio"));
+                ConnOracle.setUsuarioDB(JOptionPane.showInputDialog(null, "Inserte el usuario"));
+                ConnOracle.setPasswordDB(JOptionPane.showInputDialog(null, "Inserte la contraseña"));
             
+            }
+            Connection conn = ConnOracle.GetConnection();
+            String sql = "SELECT table_name FROM user_tables";
+            PreparedStatement ps;
+            ResultSet rs,rs2;
+            
+            CallableStatement ini;
             ps = conn.prepareStatement(sql);
             
             rs = ps.executeQuery();
             
             while(rs.next()){
-                System.out.println(rs.getString(1)+ " " + rs.getString(2));
+                String tabla=rs.getString(1);
+                String descripcion="";
+                String sql2="Select COLUMN_NAME from user_tab_columns where upper(table_name) =upper('"+rs.getString(1)+"')";
+                ps=conn.prepareStatement(sql2);
+                rs2=ps.executeQuery(sql2);
+                System.out.println("+++"+rs.getString(1)+"+++");
+                System.out.println(rs.getString(1));
+                while(rs2.next())
+                {
+                    System.out.println(rs2.getString(1));
+                    String call="{? = call estruct_column(?,?)}";
+                    ini=conn.prepareCall(call);
+                    ini.registerOutParameter(1, OracleTypes.VARCHAR);
+                    ini.setString(2, rs.getString(1));
+                    ini.setString(3, rs2.getString(1));
+                    ini.execute();
+                    System.out.print(" estruct "+ini.getString(1));
+                    System.out.println();
+                    descripcion+=ini.getString(1)+"/";
+                    
+                }
+                
+                //nivel de tabla
+                descripcion=descripcion.substring(0, descripcion.length()-1);
+                System.out.println("\n"+tabla+"\n"+descripcion+"\n");
+                MigradorBD2p1.crearTabla(tabla, descripcion);
             }
-            
+            //conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(MigradorBD2p1.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         
         
+    }
+    
+    public static void crearTabla(String tabla,String desc)
+    {
+        String upd="CREATE TABLE "+tabla+" (";
+        String[] columnas=desc.split("/");
+        String[] datos;
+        for(int i = 0;i<columnas.length;i++)
+        {
+            datos=columnas[i].split(" ");
+            //primer dato nombre de columna
+            //segundo dato tipo de dato
+            //tercer dato longitud del dato
+            if((i+1)!=columnas.length)
+                upd+=datos[0]+" "+datos[1]+"("+datos[2]+"),";
+            else
+                upd+=datos[0]+" "+datos[1]+"("+datos[2]+")";
+        }
+        
+        upd+=")";
+        
+        System.out.println("\n"+upd+"\n");
     }
     
 }
