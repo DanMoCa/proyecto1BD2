6e8028545dff555cdfac7c05726d034980d2a50e
Angek <Angek@Angek-PC>
2014-09-28 10:47:12 -0600

    se agrego el tratamiento de tipos de datos basicos

--- src/migradorbd2p1/MigradorBD2p1.java
+++ src/migradorbd2p1/MigradorBD2p1.java
@@ -89,16 +89,92 @@ public class MigradorBD2p1 {
             datos=columnas[i].split(" ");
             //primer dato nombre de columna
             //segundo dato tipo de dato
-            //tercer dato longitud del dato
-            if((i+1)!=columnas.length)
-                upd+=datos[0]+" "+datos[1]+"("+datos[2]+"),";
+            //tercer dato longitud del dato}+
+            String tipo="";
+            switch(datos[1])
+            {
+                case "NUMBER":
+                    try {
+                        CallableStatement chk;
+                        String call="{? = call GET_DSCALE(?,?)}";
+                        chk =ConnOracle.GetConnection().prepareCall(call);
+                        chk.registerOutParameter(1, OracleTypes.NUMBER);
+                        chk.setString(2, tabla);
+                        chk.setString(3, datos[0]);
+                        chk.execute();
+                        System.out.println("\n**"+chk.getInt(1)+"**\n");
+                        tipo=tratarNumber(chk.getInt(1));
+                    } catch (SQLException ex) {
+                        Logger.getLogger(MigradorBD2p1.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    break;
+                default:
+                    tipo=tratar(datos[1],datos[2]);
+            }/*
+            if(datos[1]!="NUMBER")
+                tipo=tratar(datos[1],datos[2]);
             else
-                upd+=datos[0]+" "+datos[1]+"("+datos[2]+")";
+            {
+                try {
+                    CallableStatement chk;
+                    String call="? = call GET_DSCALE(?,?)";
+                    chk =ConnOracle.GetConnection().prepareCall(call);
+                    chk.registerOutParameter(1, OracleTypes.NUMBER);
+                    chk.setString(2, tabla);
+                    chk.setString(3, datos[0]);
+                    chk.execute();
+                    System.out.println("\n**"+chk.getInt(1)+"**\n");
+                    tipo=tratarNumber(chk.getInt(1));
+                } catch (SQLException ex) {
+                    Logger.getLogger(MigradorBD2p1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            */
+            if((i+1)!=columnas.length)
+                upd+=datos[0]+" "+tipo+",";
+            else
+            {
+                upd+=datos[0]+" "+tipo;
+            }
         }
         
         upd+=")";
         
         System.out.println("\n"+upd+"\n");
+    }
+
+    private static String tratar(String tipo, String tamaño) {
+        String res="";
+        switch(tipo)
+        {
+            case "CHAR":
+            case "VARCHAR":
+                res=tipo+"("+tamaño+")";
+                break;
+            case "VARCHAR2":
+                res="VARCHAR"+"("+tamaño+")";
+                break;
+            case "DATE":
+                res="DATETIME";
+                break;
+            case "FLOAT":
+                res = tipo;
+                break;
+                
+        }
+        
+        return res.toUpperCase();
+    }
+
+    private static String tratarNumber(int aInt) {
+        if(aInt!=0)
+        {
+            return "FLOAT";
+        }
+        else
+        {
+            return "INTEGER";
+        }
     }
     
 }
